d36eb5d0651691d080650bd62d8aeed025737c7f
Devashish Sood <devashishsood9@gmail.com>
2026-02-05 18:19:23 -0500

    wilted flower emoji

--- activation_collector/__init__.py
+++ activation_collector/__init__.py
@@ -1,5 +1,5 @@
 # Activation collector for top-k activating examples per neuron
 from .config import NEURONS, LAYERS, TOP_K, NEURONS_BY_LAYER
 from .heap_tracker import NeuronHeapTracker, ActivationRecord
-from .export import export_to_parquet, export_combined_parquet
+from .export import export_to_parquet 
 from .collector import collect_activations
